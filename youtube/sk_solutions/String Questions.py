5871a530eb17872cf0f47761e9ed71dd2571bcd8
CaptainSK2812 <sujanshettybangari28@gmail.com>
2025-09-03 15:47:46 +0530

    Add files via upload

---
+++ youtube/sk_solutions/String Questions.py
@@ -0,0 +1,19 @@
+# Write a Python program to reverse a given string.
+
+# Write a program to check if a given string is a palindrome.
+
+# Write a program that counts the number of vowels in a given string.
+
+# Write a program that counts the number of words in a given sentence.
+
+# Write a program to convert a string to uppercase.
+
+# Write a Python program to replace all spaces in a string with underscores.
+
+# Write a program that removes all vowels from a given string.
+
+# Write a program to count the frequency of each character in a string.
+
+# Write a program that checks if two input strings are anagrams of each other.
+
+# Write a program to capitalize the first letter of each word in a sentence.
